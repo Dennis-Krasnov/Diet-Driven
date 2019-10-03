e763f8978e365baaa30037db3cbd2f5ffc25595b
Dennis Krasnov <Dennis_Krasnov@ultimatesoftware.com>
2019-10-02 20:01:15 -0400

    redo android and ios configurations

--- flutter_app/ios/Flutter/flutter_export_environment.sh
+++ flutter_app/ios/Flutter/flutter_export_environment.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/Users/dennisk/Dev/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/dennisk/Dev/fitness-driven/diet_driven"
-export "FLUTTER_TARGET=/Users/dennisk/Dev/fitness-driven/diet_driven/lib/main.dart"
+export "FLUTTER_APPLICATION_PATH=/Users/dennisk/Dev/diet_driven/flutter_app"
+export "FLUTTER_TARGET=/Users/dennisk/Dev/diet_driven/flutter_app/lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
 export "FLUTTER_FRAMEWORK_DIR=/Users/dennisk/Dev/flutter/bin/cache/artifacts/engine/ios"
