3418589bfadd3ce6cb6932401e0b76820ebef532
Dennis Krasnov <dennis@krasnov.dev>
2021-01-30 17:55:13 -0500

    add firebase for android

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
 import 'package:dietdriven/widget/diet_driven_app_provider_wrapper.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:provider/provider.dart';
 import 'package:flutter/material.dart';
 
-void main() {
-  // ...
+void main() async {
+  // Initialize Firebase SDK
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
 
   // Initialize Flutter application
   runApp(DietDrivenAppProviderWrapper());
